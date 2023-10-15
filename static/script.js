ca4e944d83b30dbe733929cbc9dc93cdb3682dc1
aoife lennon <lennonaoife5@gmail.com>
2023-10-15 17:15:52 +0100

    fifth commit

--- static/script.js
+++ static/script.js
@@ -86,65 +86,118 @@ document.getElementById('login-form').addEventListener('submit', function (e) {
 
 // calendar.js
 
-let currentMonth = document.querySelector(".current-month");
-let calendarDays = document.querySelector(".calendar-days");
-let today = new Date();
 let date = new Date();
+let year = date.getFullYear();
+let month = date.getMonth();
 
+const day = document.querySelector(".calendar-dates");
 
-currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-today.setHours(0,0,0,0);
-renderCalendar();
+const currdate = document
+	.querySelector(".calendar-current-date");
 
-function renderCalendar(){
-    const prevLastDay = new Date(date.getFullYear(),date.getMonth(),0).getDate();
-    const totalMonthDay = new Date(date.getFullYear(),date.getMonth()+1,0).getDate();
-    const startWeekDay = new Date(date.getFullYear(),date.getMonth(),0).getDay();
-    
-    calendarDays.innerHTML = "";
+const prenexIcons = document
+	.querySelectorAll(".calendar-navigation span");
 
-    let totalCalendarDay = 6 * 7;
-    for (let i = 0; i < totalCalendarDay; i++) {
-        let day = i-startWeekDay;
+// Array of month names
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
 
-        if(i <= startWeekDay){
-            // adding previous month days
-            calendarDays.innerHTML += `<div class='padding-day'>${prevLastDay-i}</div>`;
-        }else if(i <= startWeekDay+totalMonthDay){
-            // adding this month days
-            date.setDate(day);
-            date.setHours(0,0,0,0);
-            
-            let dayClass = date.getTime()===today.getTime() ? 'current-day' : 'month-day';
-            calendarDays.innerHTML += `<div class='${dayClass}'>${day}</div>`;
-        }else{
-            // adding next month days
-            calendarDays.innerHTML += `<div class='padding-day'>${day-totalMonthDay}</div>`;
-        }
-    }
+// Function to generate the calendar
+const manipulate = () => {
+
+	// Get the first day of the month
+	let dayone = new Date(year, month, 1).getDay();
+
+	// Get the last date of the month
+	let lastdate = new Date(year, month + 1, 0).getDate();
+
+	// Get the day of the last date of the month
+	let dayend = new Date(year, month, lastdate).getDay();
+
+	// Get the last date of the previous month
+	let monthlastdate = new Date(year, month, 0).getDate();
+
+	// Variable to store the generated calendar HTML
+	let lit = "";
+
+	// Loop to add the last dates of the previous month
+	for (let i = dayone; i > 0; i--) {
+		lit +=
+			`<li class="inactive">${monthlastdate - i + 1}</li>`;
+	}
+
+	// Loop to add the dates of the current month
+	for (let i = 1; i <= lastdate; i++) {
+
+		// Check if the current date is today
+		let isToday = i === date.getDate()
+			&& month === new Date().getMonth()
+			&& year === new Date().getFullYear()
+			? "active"
+			: "";
+		lit += `<li class="${isToday}">${i}</li>`;
+	}
+
+	// Loop to add the first dates of the next month
+	for (let i = dayend; i < 6; i++) {
+		lit += `<li class="inactive">${i - dayend + 1}</li>`
+	}
+
+	// Update the text of the current date element 
+	// with the formatted current month and year
+	currdate.innerText = `${months[month]} ${year}`;
+
+	// update the HTML of the dates element 
+	// with the generated calendar
+	day.innerHTML = lit;
 }
 
-document.querySelectorAll(".month-btn").forEach(function (element) {
-	element.addEventListener("click", function () {
-		date = new Date(currentMonth.textContent);
-        date.setMonth(date.getMonth() + (element.classList.contains("prev") ? -1 : 1));
-		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-		renderCalendar();
-	});
-});
+manipulate();
 
-document.querySelectorAll(".btn").forEach(function (element) {
-	element.addEventListener("click", function () {
-        let btnClass = element.classList;
-        date = new Date(currentMonth.textContent);
-        if(btnClass.contains("today"))
-            date = new Date();
-        else if(btnClass.contains("prev-year"))
-            date = new Date(date.getFullYear()-1, 0, 1);
-        else
-            date = new Date(date.getFullYear()+1, 0, 1);
-        
-		currentMonth.textContent = date.toLocaleDateString("en-US", {month:'long', year:'numeric'});
-		renderCalendar();
+// Attach a click event listener to each icon
+prenexIcons.forEach(icon => {
+
+	// When an icon is clicked
+	icon.addEventListener("click", () => {
+
+		// Check if the icon is "calendar-prev"
+		// or "calendar-next"
+		month = icon.id === "calendar-prev" ? month - 1 : month + 1;
+
+		// Check if the month is out of range
+		if (month < 0 || month > 11) {
+
+			// Set the date to the first day of the 
+			// month with the new year
+			date = new Date(year, month, new Date().getDate());
+
+			// Set the year to the new year
+			year = date.getFullYear();
+
+			// Set the month to the new month
+			month = date.getMonth();
+		}
+
+		else {
+
+			// Set the date to the current date
+			date = new Date();
+		}
+
+		// Call the manipulate function to 
+		// update the calendar display
+		manipulate();
 	});
 });

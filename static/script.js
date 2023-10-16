259dd28a8c0381ee19e646226966d2d5b013c4f8
aoife lennon <lennonaoife5@gmail.com>
2023-10-16 19:57:11 +0100

    edit

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
 // script.js
 
+//task bar 
 const taskInput = document.getElementById('task-input');
 const priorityDropdown = document.getElementById('priority-dropdown');
 const addTaskButton = document.getElementById('add-task');
@@ -34,41 +35,6 @@ addTaskButton.addEventListener('click', () => {
         taskInput.value = '';
         priorityDropdown.value = 'low'; 
     }
-});
-document.getElementById('login-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    const username = document.querySelector('input[name="username"]').value;
-    const password = document.querySelector('input[name="password"]').value;
-
-   
-    // Prepare the data to send to the server
-    const data = {
-        username: email,
-        password: password
-    };
-
-    // Send data to the server for validation
-    fetch('/validate-login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Handle the response from the server
-        if (data.success) {
-            // Redirect to the main page on successful login
-            window.location.href = 'main.html';
-        } else {
-            // Handle unsuccessful login (e.g., display an error message)
-            alert('Login failed. Please check your credentials.');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
 });
 
 
@@ -105,22 +71,21 @@ const months = [
 // Function to generate the calendar
 const manipulate = () => {
 
-	// Get the first day of the month
 	let dayone = new Date(year, month, 1).getDay();
 
-	// Get the last date of the month
+	
 	let lastdate = new Date(year, month + 1, 0).getDate();
 
-	// Get the day of the last date of the month
+	
 	let dayend = new Date(year, month, lastdate).getDay();
 
-	// Get the last date of the previous month
+	
 	let monthlastdate = new Date(year, month, 0).getDate();
 
-	// Variable to store the generated calendar HTML
+	
 	let lit = "";
 
-	// Loop to add the last dates of the previous month
+	
 	for (let i = dayone; i > 0; i--) {
 		lit +=
 			`<li class="inactive">${monthlastdate - i + 1}</li>`;
@@ -144,7 +109,7 @@ const manipulate = () => {
 	}
 
 	// Update the text of the current date element 
-	// with the formatted current month and year
+	// with the current month and year
 	currdate.innerText = `${months[month]} ${year}`;
 
 	// update the HTML of the dates element 
@@ -154,21 +119,17 @@ const manipulate = () => {
 
 manipulate();
 
-// Attach a click event listener to each icon
+
 prenexIcons.forEach(icon => {
 
-	// When an icon is clicked
 	icon.addEventListener("click", () => {
 
 		// Check if the icon is "calendar-prev"
 		// or "calendar-next"
 		month = icon.id === "calendar-prev" ? month - 1 : month + 1;
 
-		// Check if the month is out of range
 		if (month < 0 || month > 11) {
             
-			// Set the date to the first day of the 
-			// month with the new year
 			date = new Date(year, month, new Date().getDate());
 
 			// Set the year to the new year
@@ -183,9 +144,6 @@ prenexIcons.forEach(icon => {
 			// Set the date to the current date
 			date = new Date();
 		}
-
-		// Call the manipulate function to 
-		// update the calendar display
 		manipulate();
 	});
 });
@@ -202,17 +160,16 @@ document.addEventListener("DOMContentLoaded", function() {
     saveEntryButton.addEventListener("click", function() {
         const currentDate = new Date();
         currentDateElement.textContent = currentDate.toDateString();
-        // You can save the entry content to a database or storage here.
     });
 
     // Function to navigate to the previous entry
     previousEntryButton.addEventListener("click", function() {
-        // Implement logic to load the previous journal entry here.
+       
     });
 
     // Function to navigate to the next entry
     nextEntryButton.addEventListener("click", function() {
-        // Implement logic to load the next journal entry here.
+    
     });
 });
 document.addEventListener("DOMContentLoaded", function() {
@@ -256,17 +213,12 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Function to navigate to the previous entry
     previousEntryButton.addEventListener("click", function() {
-        // Implement logic to load the previous journal entry here.
-        // This logic will depend on how you store and organize your entries.
-        // You may use dates or an array to manage entries.
+   
     });
 
     // Function to navigate to the next entry
     nextEntryButton.addEventListener("click", function() {
-        // Implement logic to load the next journal entry here.
-        // Similar to the previous entry, the logic will depend on your data structure.
+    
     });
-
-    // Load the current entry when the page loads
     loadCurrentEntry();
 });
